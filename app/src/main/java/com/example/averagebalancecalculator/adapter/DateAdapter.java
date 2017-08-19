24a394fe3da0ee7c1a025a51d686be36bd7c4221
DESKTOP-EVGGAP4\SULTAN <salim.acmeuniverse@gmail.com>
2017-08-19 13:30:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/averagebalancecalculator/adapter/DateAdapter.java
@@ -0,0 +1,27 @@
+package com.example.averagebalancecalculator.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ *
+ * Created by SULTAN on 05-08-2017.
+ *
+ */
+
+public class DateAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
