2786351130723b78bd5930cde441a7568c79594b
DESKTOP-EVGGAP4\SULTAN <salim.acmeuniverse@gmail.com>
2017-10-27 19:03:09 +0530

    Layout bugs and days issue

--- app/src/main/java/com/example/averagebalancecalculator/MainActivity.java
+++ app/src/main/java/com/example/averagebalancecalculator/MainActivity.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.example.averagebalancecalculator.adapter.DateAdapter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mAccountNumber;
     private RecyclerView mRecyclerViewDate;
+    private List<Integer> mDateList;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,10 +48,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAccountNumber = (EditText) findViewById(R.id.account_number);
+        mAccountNumber = findViewById(R.id.account_number);
+
+        int day = numberOfDaysInMonth();
+        int days[];
+
+        //= new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+
+//        mDateList = new ArrayList<>(day);
+//        mDateList.add(new Date());
+
+        Log.e("days", String.valueOf(day));
 //        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 //        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        mRecyclerViewDate = (RecyclerView) findViewById(R.id.balance_calculator);
+        mRecyclerViewDate = findViewById(R.id.balance_calculator);
+        mRecyclerViewDate.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        try {
+            days = new int[day];
+            List<Integer> listDays = new ArrayList<>();
+            for (int i = 0; i < days.length; i++) {
+                Log.e("i", String.valueOf(i + 1));
+//            mDateList = new ArrayList<>();
+//            mDateList.add(i);
+
+                listDays.add(i, i + 1);
+
+                days[i] = i + 1;
+
+//                Log.e("days[" + i + "]", String.valueOf(listDays.get(i + 1)));
+                if (mRecyclerViewDate.getAdapter() == null) {
+                    mRecyclerViewDate.setAdapter(new DateAdapter(listDays, R.layout.row_item, getApplicationContext()));
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException aIOoBE) {
+            aIOoBE.printStackTrace();
+            Log.e("aIOoBE", aIOoBE.toString());
+        } catch (IndexOutOfBoundsException iOoBE) {
+            iOoBE.printStackTrace();
+            Log.e("iOoBE", String.valueOf(iOoBE));
+        }
+    }
+
+    /***
+     *
+     * @return
+     * Returns the total number of days in the current month
+     *
+     */
+    public static int numberOfDaysInMonth() {
+        Calendar monthStart = Calendar.getInstance();
+        return monthStart.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 
 }
--- app/src/main/java/com/example/averagebalancecalculator/adapter/DateAdapter.java
+++ app/src/main/java/com/example/averagebalancecalculator/adapter/DateAdapter.java
@@ -1,27 +1,91 @@
 package com.example.averagebalancecalculator.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.averagebalancecalculator.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
- *
  * Created by SULTAN on 05-08-2017.
- *
  */
 
-public class DateAdapter extends RecyclerView.Adapter {
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class DateAdapter extends RecyclerView.Adapter<DateAdapter.DateViewHolder> {
+
+    private List<Integer> mDate;
+    //    private int mDay;
+    private int mLayout;
+    private Context mContext;
+
+    public DateAdapter(List<Integer> date, int dateLayout, Context context) {
+        //int day,
+        mDate = date;
+//        mDay = new ArrayList<>();
+//        mDay = day;
+        mLayout = dateLayout;
+        mContext = context;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public DateViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(mLayout, parent, false);
+        return new DateViewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(DateViewHolder holder, int position) {
+        holder.etDate.setText(String.valueOf(mDate.get(position)));
+        Log.e("day", String.valueOf(mDate.get(position)));
+
+        double averageBalance[] = new double[position];
+//        double openingBalance;
+        double closingBalance[] = new double[position];
+        double sumBalance = 0.00, temp = 0.00;
+
+//        openingBalance = Float.parseFloat(holder.tvOpeningBalance.getText().toString());
+        if (!holder.etClosingBalance.getText().toString().equals("")) {
+            closingBalance[position] = Double.valueOf(holder.etClosingBalance.getText().toString());
+            averageBalance[position] = 0.00;
+
+            for (int i = 0; i < position; i++) {
+                sumBalance += closingBalance[i];
+                averageBalance[i] = sumBalance / position;
+
+                temp = averageBalance[i];
+            }
+        }
+        holder.etAverageBalance.setText(String.valueOf(temp));
+
+    }
+
+    public float calculateAverageBalance(float balanceStart, float balanceEnd, int date) {
+        return (balanceStart + balanceEnd) / date;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDate.size();
+    }
+
+    public static class DateViewHolder extends RecyclerView.ViewHolder {
+
+        TextView etDate;
+        EditText etOpeningBalance, etClosingBalance, etAverageBalance;
+
+        public DateViewHolder(View itemView) {
+            super(itemView);
+            etDate = itemView.findViewById(R.id.date_item);
+//            tvOpeningBalance = (EditText) itemView.findViewById(R.id.opening_balance_item);
+            etClosingBalance = itemView.findViewById(R.id.closing_balance_item);
+            etAverageBalance = itemView.findViewById(R.id.average_balance_item);
+        }
     }
 }
